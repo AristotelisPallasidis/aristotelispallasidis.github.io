5d8ab0c5d854ee768db04333202e8297b0fca9ca
Aristotelis Pallasidis <aristotelis.pallasid@gmail.com>
2022-12-11 00:20:45 +0200

    the new website

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#project1',{
+    duration: 2000,
+    delay: 100,
+    origin: 'top',
+    distance: '50px',
+    viewFactor: 0.2
+})
+sr.reveal('#project2',{
+    duration: 2000,
+    delay: 300,
+    origin: 'top',
+    distance: '50px',
+    viewFactor: 0.2
+})
+sr.reveal('#project3',{
+    duration: 2000,
+    delay: 200,
+    origin: 'top',
+    distance: '100px',
+    viewFactor: 0.2
+})
+sr.reveal('#project4',{
+    duration: 2000,
+    delay: 300,
+    origin: 'top',
+    distance: '100px',
+    viewFactor: 0.2
+})
---
+++ js/me.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#about', {
+    duration: 3000,
+    delay: 300,
+    origin: 'top',
+    distance: '50px',
+    viewFactor: 0.2
+})

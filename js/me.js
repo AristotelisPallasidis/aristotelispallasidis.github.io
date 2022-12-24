d8d20c78a37a225cfc01b83538cd6f06bf9befa8
Aristotelis Pallasidis <aristotelis.pallasid@gmail.com>
2022-12-24 14:09:16 +0200

    Finished project 01 pics etc

--- js/me.js
+++ js/me.js
@@ -1,8 +1,8 @@
 window.sr = ScrollReveal();
 
 sr.reveal('#about', {
-    duration: 3000,
-    delay: 300,
+    duration: 1500,
+    delay: 200,
     origin: 'top',
     distance: '50px',
     viewFactor: 0.2
